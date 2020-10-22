3a506c37ebf5f25c3e3737f38dea6d22ff5fd00b
newall <newalljemuel.d@gmail.com>
2020-10-22 08:39:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+function App(){
+    return(
+
+      <div>
+
+
+      {/* Nav elements and Self introduction section */}
+
+
+
+      {/* Technologies I know section  */}
+
+
+
+      {/* Skill level section  */}
+
+
+
+      {/* My works section */}
+
+
+
+      {/* Contact me section */}
+
+  
+
+      </div>
+    )
+}
+
+export default App;
+
+
+
